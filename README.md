e6c6760aaff75937d0ff09364e96da686ba41e37
Erick Sandhu <essandhu22@gmail.com>
2024-08-31 00:21:15 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Scoutly
+
+Track prices of your favorite Amazon products and get alerts when they go on sale
+
+## Features
+
+
+## Tech Stack
+
+
+## Acknowledgements
---
+++ app/page.tsx
@@ -0,0 +1,53 @@
+import HeroCarousel from "@/components/HeroCarousel";
+import Searchbar from "@/components/Searchbar";
+import Image from "next/image";
+import { getAllProducts } from "@/lib/actions";
+import ProductCard from "@/components/ProductCard";
+
+const Home = async () => {
+  const allProducts = await getAllProducts();
+
+  return (
+    <>
+      <section className="px-6 md:px-20 py-24">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">
+              Ready to Start Saving Money?
+              <Image
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
+            <h1 className="head-text">
+              Introducing
+              <span className="text-primary"> Scoutly</span>
+            </h1>
+            <p className="text-2xl text-black opacity-50 mt-2">
+              Your online shopping companion
+            </p>
+            <p className="mt-6">
+              Easy access to price data and discount analytics to help you win
+              big when browsing your favorite products.
+            </p>
+            <Searchbar />
+          </div>
+          <HeroCarousel />
+        </div>
+      </section>
+
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
